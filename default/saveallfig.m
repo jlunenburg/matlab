2c900efbe0f1e861e317a7f7ed50b7082f2cb60e
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-12-17 15:16:32 +0100

    Added saveallfig function

---
+++ default/saveallfig.m
@@ -0,0 +1,38 @@
+function saveallfig(name,handle,path) 
+%path settings: You can give your favorite paths here, I always set it to the pictures folder of my current project
+%               just enter a number in the 'path' input instead of the path
+preferredpath{1}='D:\Thesis\'; 
+%preferredpath{2}='C:\Pictures\;
+%etc,etc...'
+
+%use current figure if no handle is supplied 
+if (nargin<2) || isempty(handle)
+    handle=gcf; 
+end 
+%default path current dir 
+if (nargin<3) || isempty(path) 
+    if(isunix)         
+        path=[cd '/']; %for linux users 
+    else 
+        path=[cd '\']; %for windows users 
+    end
+else
+    if isnumeric(path) %if path is one of the preferred paths
+        path=preferredpath{path};     
+    else
+        %do nothing, path given in function input is the desired path
+    end       
+end
+
+% Save figure
+saveas(handle, strcat(path, strcat('/', strcat(name,'.fig') ) ) );
+
+% Save pdf
+saver(name, handle, path);
+
+% Save tikz
+matlab2tikz('figurehandle',handle,'filename', strcat(path, strcat('/', strcat(name,'.tikz') ) ),...
+    'height','\figureheight','width','\figurewidth');
+
+%% Possible issues
+
